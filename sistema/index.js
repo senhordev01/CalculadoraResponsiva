14d7b9a985ebd625fbdc3933d5c30181410473c5
Rogerio <contaprogramacao230@gmail.com>
2026-07-12 18:49:49 -0300

    Commit da logica do sistema

--- sistema/index.js
+++ sistema/index.js
@@ -1,28 +1,22 @@
-// function Sistema(){
-//     let numero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//     document.getElementById("Resultado-Final").innerHTML = numero;
-// }
 
-// function Limpar(){
-//     let limpar = 0;
-//     document.getElementById("Resultado-Final").innerHTML = limpar;
-// }
+const numero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const Resultado = document.getElementById("Resultado-Final");
 
-class Calculadora{
-    constructor(numero){
-        this.numero = numero;
-    }
-
-    Sistema(){
-        let numero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        document.getElementById("Resultado-Final").innerHTML = numero;
-    }
-    Limpar(){
-        let limpar = 0;
-        document.getElementById("Resultado-Final").innerHTML = limpar;
-    }
+function Num_Inicial(){
+   Resultado.textContent = numero[0]
 }
 
-const calculadora = new Calculadora();
-calculadora.Sistema();
-calculadora.Limpar();
+function Sistema(indice){
+    if (Resultado.textContent === "0") {
+        Resultado.textContent = numero[indice];
+        
+    }else {
+        Resultado.textContent += numero[indice];
+    } 
+}
+document.addEventListener("DOMContentLoaded", Num_Inicial);
+
+function Limpar(){
+    let limpar = 0;
+    document.getElementById("Resultado-Final").innerHTML = limpar;
+}
