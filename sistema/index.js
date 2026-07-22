83d0a72b6fa1a95592e14fb7aba1d063472b9194
Rogerio <contaprogramacao230@gmail.com>
2026-07-22 00:26:46 -0300

    Operadores funcionando!

--- sistema/index.js
+++ sistema/index.js
@@ -1,5 +1,6 @@
 
 const numero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operadores = ["x", "÷", "+", "-","%"]
 const Resultado = document.getElementById("Resultado-Final");
 
 function Num_Inicial(){
@@ -7,16 +8,44 @@ function Num_Inicial(){
 }
 
 function Sistema(indice){
-    if (Resultado.textContent === "0") {
-        Resultado.textContent = numero[indice];
+
+    try{
+        if(Resultado.textContent.length > 15){
+            return;
+        }if (Resultado.textContent === "0") {
+            Resultado.textContent = numero[indice];
+            
+        }else{
+            Resultado.textContent += numero[indice];
+
+        }
+
         
-    }else {
-        Resultado.textContent += numero[indice];
-    } 
+    }catch(erro){
+        console.log(erro.message);
+    }
+
 }
 document.addEventListener("DOMContentLoaded", Num_Inicial);
 
 function Limpar(){
     let limpar = 0;
     document.getElementById("Resultado-Final").innerHTML = limpar;
+}
+
+function operacoes(operador){
+    try{
+        const ultimo = Resultado.textContent.slice(-1);
+
+        if(!operadores.includes(ultimo)){
+            Resultado.textContent += operador;
+        }
+
+    }catch(erro){
+        console.log(erro.message);
+    }
+}
+
+function Calcular(){
+    console.log("")
 }
